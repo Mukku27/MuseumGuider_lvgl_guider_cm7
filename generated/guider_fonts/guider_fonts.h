ca177ad03a6b3f9a7f11e880d5bd72dbf5ff3ab8
VemulapalliMukesh27 <vemulapallimukesh@gmail.com>
2024-12-08 17:07:07 +0530

    Add new font files for Acme and Montserrat in Guider project

---
+++ generated/guider_fonts/guider_fonts.h
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2024 NXP
+ * SPDX-License-Identifier: MIT
+ * The auto-generated can only be used on NXP devices
+ */
+#ifndef GUIDER_FONTS_H
+#define GUIDER_FONTS_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if LVGL_VERSION_MAJOR == 7
+#include "lv_font/lv_font.h"
+#else
+#include "font/lv_font.h"
+#endif
+
+LV_FONT_DECLARE(lv_font_Acme_Regular_26)
+LV_FONT_DECLARE(lv_font_montserratMedium_16)
+LV_FONT_DECLARE(lv_font_montserratMedium_12)
+LV_FONT_DECLARE(lv_font_Acme_Regular_40)
+LV_FONT_DECLARE(lv_font_Acme_Regular_28)
+LV_FONT_DECLARE(lv_font_Acme_Regular_32)
+LV_FONT_DECLARE(lv_font_Acme_Regular_20)
+LV_FONT_DECLARE(lv_font_Abel_regular_20)
+LV_FONT_DECLARE(lv_font_Acme_Regular_29)
+LV_FONT_DECLARE(lv_font_Abel_regular_25)
+LV_FONT_DECLARE(lv_font_Alatsi_Regular_35)
+LV_FONT_DECLARE(lv_font_Acme_Regular_30)
+LV_FONT_DECLARE(lv_font_montserratMedium_30)
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif
